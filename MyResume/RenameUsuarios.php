27b01744200c0a25aea5518ba67acdc688eaf8f9
Estefi Gimenez <estefi333gimenez@gmail.com>
2022-08-23 17:41:27 -0300

    cambios del crud en el template

---
+++ MyResume/RenameUsuarios.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'conexion.php';
+
+$id = $_POST['idAV'];
+$nom_comp = $_POST['nombreCompletoAV'];
+$dni = $_POST['dniAV'];
+$email = $_POST['emailAV'];
+$contra = $_POST['contrasennaAV'];
+$tipo = $_POST['tipoUsuarioAV'];
+
+
+
+
+$q = "UPDATE usuarios SET nombre_completo='$nom_comp',email='$email',dni='$dni',contrasenna='$contra',tipo_usuario='$tipo' WHERE id_usuario = '$id'";
+
+echo mysqli_query($con, $q);
+
+
+?>
