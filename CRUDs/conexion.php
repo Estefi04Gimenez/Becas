6d6718a65bd9d60f20155fb96fb6e8b2929331b5
Simón Rossini <simonrossin@gmail.com>
2022-08-24 19:44:13 -0300

    xd

---
+++ CRUDs/conexion.php
@@ -0,0 +1,11 @@
+<?php
+//parametros para la conexión
+$dbip = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "becas";
+//conexió
+//api mysql para php
+$con = mysqli_connect($dbip, $dbuser, $dbpass, $dbname);
+
+?>
